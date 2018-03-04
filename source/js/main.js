2d143608f2ce136f0c5fd851c39d3350efefa0e3
Pozharov Roman <ru.roman.ps@gmail.com>
2018-03-04 18:04:04 +0400

    js in form

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,15 @@
 var open = document.querySelector(".header__open");
 var close = document.querySelector(".header__close");
 var menu = document.querySelector(".header__menu");
-var popup = document.querySelector(".popup-success");
 var popupBtn = document.querySelector(".form__submit-btn");
-var popupClose = document.querySelector(".popup-success__btn-close");
+var popup = document.querySelector(".popup-success");
+var popupNo = document.querySelector(".popup");
+var popupClose = popup.querySelector(".popup-success__btn-close");
+var popupNoClose = popupNo.querySelector(".popup__btn");
+var form = document.querySelector("form");
+var name = document.querySelector("[name=name]");
+var surname = document.querySelector("[name=surname]");
+var mail = document.querySelector("[name=email]");
 
 
 open.addEventListener("click", function(event) {
@@ -20,12 +26,33 @@ close.addEventListener("click", function(event) {
   open.classList.remove("header__btn-hide");
 });
 
-popupBtn.addEventListener("click", function(event) {
+form.addEventListener("submit", function(event) {
+  if ((!name.value) || (!surname.value) || (!mail.value)) {
+    event.preventDefault();
+    popupNo.classList.add("popup__show");
+  } else {
+    event.preventDefault();
+    popup.classList.add("popup-success__show");
+  }
+});
+
+popupNoClose.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.add("popup-success__show");
+  popupNo.classList.remove("popup__show");
 });
 
 popupClose.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.remove("popup-success__show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popupNo.classList.contains("popup__show")) {
+      popupNo.classList.remove("popup__show");
+    }
+    if (popup.classList.contains("popup-success__show")) {
+      popup.classList.remove("popup-success__show");
+    }
+  }
 });
